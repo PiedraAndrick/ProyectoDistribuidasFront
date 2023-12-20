8a168e8bfe152dfb5a697971cca55b6675ec4bbb
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-12-19 23:55:28 -0500

    Add files via upload

---
+++ Front/src/app/institucion/institucion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ServiceService } from '../servicio/service.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import swal from 'sweetalert2'
+
+@Component({
+  selector: 'app-institucion',
+  templateUrl: './institucion.component.html',
+  styleUrls: ['./institucion.component.scss']
+})
+export class InstitucionComponent implements OnInit {
+
+  
+}
