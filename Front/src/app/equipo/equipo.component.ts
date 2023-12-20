ffbb85fcc4496a4efad7611c7aceae8699bc9281
MaylonCuzco <65976107+MaylonCuzco@users.noreply.github.com>
2023-12-19 23:51:27 -0500

    Add files via upload

---
+++ Front/src/app/equipo/equipo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ServiceService } from '../servicio/service.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import swal from 'sweetalert2'
+
+@Component({
+  selector: 'app-equipo',
+  templateUrl: './equipo.component.html',
+  styleUrls: ['./equipo.component.scss']
+})
+export class EquipoComponent implements OnInit {
+
+ 
+}
